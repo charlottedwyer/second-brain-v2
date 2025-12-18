253ba6e7c32f9766b8222656b48e891d74baaabb
Charlotte <dwycharlotte@gmail.com>
2025-12-18 03:47:13 +0000

    chore: temporary rename media and wiki files

---
+++ src/pages/Wiki_tmp.tsx
@@ -0,0 +1,10 @@
+export default function Wiki() {
+  return (
+    <div>
+      <h2>Wiki</h2>
+      <p style={{ opacity: 0.7 }}>
+        Worldbuilding, notes, concepts, references.
+      </p>
+    </div>
+  );
+}
